a155747eba9c1ea5e7a524b3f4a90c4e2a042f90
Humoyun2005 <abduodirovhumoyun@gmail.com>
2023-04-04 18:18:49 +0500

    second

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-       
+       fasdfasd;flkjdsaf;kasdjl;ka
       </main>
     </>
   )
