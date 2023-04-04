4be72d86469266a58afc3a27f6ac976180946e95
Humoyun2005 <abduodirovhumoyun@gmail.com>
2023-04-04 18:22:42 +0500

    anywhere

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from 'next/font/google'
+
+
+
 
 export default function Home() {
   return (
