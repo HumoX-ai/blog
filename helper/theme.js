b16dbad72c87cfd0bd880a9b8dbd929cb4d98307
Humoyun2005 <abduodirovhumoyun@gmail.com>
2023-04-05 11:45:42 +0500

    2

--- helper/theme.js
+++ helper/theme.js
@@ -1,18 +1,20 @@
-
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
 import { Roboto } from "next/font/google";
 
 export const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
+    background: {
+      default: "#0a1929",
+    },
     primary: {
       main: "#fff",
     },
